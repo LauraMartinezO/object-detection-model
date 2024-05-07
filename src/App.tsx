64301af688c3e3c2bd694f050ea0edbadbfd4c3f
LauraMartinezO <lauramartinezo182@gmail.com>
2024-05-06 23:04:25 -0500

    Feat: Video and files are displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,107 @@
 import "./App.css";
 
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Box, CardMedia } from "@mui/material";
+import { useState, useRef } from "react";
 import {
   FileUploadOutlined,
   PlayCircleOutlineRounded,
 } from "@mui/icons-material";
-import { ChangeEvent } from "react";
 
 import CardComponent from "./components/CardComponent";
+import Webcam from "react-webcam";
 
 function App() {
-  const uploadFile = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files ? e.target.files[0] : null;
+  const [uploadedFile, setUploadedFile] = useState(null);
+  const [showWebcam, setShowWebcam] = useState(false);
+  const webcamRef = useRef(null);
+
+  const uploadFile = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setUploadedFile(URL.createObjectURL(file));
+    }
     console.log("Archivo: ", file?.name);
   };
 
-  async function openCamera(e) {
-    e.preventDefault();
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      console.log("Stream prendida: ", stream);
-    } catch (error) {
-      console.error("Error al acceder a la cámara: ", error);
-    }
+  const openCamera = () => {
+    setShowWebcam(!showWebcam);
   }
 
   return (
-    <div className="container">
-      <CardComponent title="Object Detection Input">
-        <Grid container justifyContent="center" marginTop={5} columnSpacing={6}>
-          <Grid item>
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: "#424B5A",
-                "&:hover": { backgroundColor: "#424B5A" },
-              }}
-              component="label"
-              endIcon={<FileUploadOutlined />}
+    <Box sx={{ flexGrow: 1, margin: 4.2 }}>
+      <Grid container columns={1} rowGap={5}>
+        <Grid item xs={8}>
+          <CardComponent height={155} title="Object Detection Input">
+            <Grid
+              container
+              justifyContent="center"
+              marginTop={5}
+              columnSpacing={6}
             >
-              Upload File
-              <input
-                type="file"
-                name="image"
-                accept="image/png, image/jpeg"
-                onChange={uploadFile}
-                hidden
-              />
-            </Button>
-          </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "#424B5A",
+                    "&:hover": { backgroundColor: "#424B5A" },
+                  }}
+                  component="label"
+                  endIcon={<FileUploadOutlined />}
+                >
+                  Upload File
+                  <input
+                    type="file"
+                    name="image"
+                    accept="image/png, image/jpeg"
+                    onChange={uploadFile}
+                    hidden
+                  />
+                </Button>
+              </Grid>
 
-          <Grid item>
-            <Button
-              variant="outlined"
-              onClick={openCamera}
-              endIcon={<PlayCircleOutlineRounded />}
-            >
-              Open camera
-            </Button>
-          </Grid>
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  onClick={openCamera}
+                  endIcon={<PlayCircleOutlineRounded />}
+                >
+                  {showWebcam ? "Close camera" : "Open camera"}
+                </Button>
+              </Grid>
+            </Grid>
+          </CardComponent>
         </Grid>
-      </CardComponent>
-    </div>
+
+        <Grid item xs={4}>
+          <CardComponent height={530} title="Results">
+            {showWebcam ? (
+              <Webcam
+                ref={webcamRef}
+                style={{
+                  width: "100%",
+                  height: "430px",
+                  margin: "1.5rem",
+                  objectFit: "contain",
+                }}
+              />
+            ) : (
+              uploadedFile && (
+                <CardMedia
+                  component="img"
+                  src={uploadedFile}
+                  style={{
+                    width: "100%",
+                    height: "430px",
+                    margin: "1.5rem",
+                    objectFit: "contain",
+                  }}
+                />
+              )
+            )}
+          </CardComponent>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -2,9 +2,15 @@ import Divider from "@mui/material/Divider";
 import Card from "@mui/material/Card";
 import PropTypes from "prop-types";
 
-const CardComponent = ({ title, children  }) => {
+interface CardComponentProps {
+  title: string;
+  height: number;
+  children: React.ReactNode;
+}
+
+const CardComponent = ({ title, height, children }: CardComponentProps) => {
   return (
-    <Card sx={{ width: 1400, height: 200 }}>
+    <Card sx={{ width: 1400, height: height }}>
       <h2 className="container__title">{title}</h2>
       <Divider />
       <div>{children}</div>
